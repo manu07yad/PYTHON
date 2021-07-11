61e719bbf30a12fa80c9d5bfd93d8dd2bbf5ce5c
MANASVI YADAV <67043791+manu07yad@users.noreply.github.com>
2021-07-12 00:08:26 +0530

    Add files via upload

---
+++ to check two files are identical or not.py
@@ -0,0 +1,12 @@
+file1= "name.txt"
+file2= "copy2.txt"
+with open(file1) as f:
+     f1= f.read()
+
+with open (file2) as f:
+     f2= f.read()
+
+if f1==f2:
+    print("Files are identical")
+else:
+    print("Files are not identical") 
